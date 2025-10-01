354b4ea6a3025a16a7cee8afa3243a93ce40de84
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-10-01 13:30:46 +0530

    Config SetUp

---
+++ src/main/java/lk/ijse/gdse70/agreebloom/AgreeBloomBackendMainApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse70.agreebloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgreeBloomBackendMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgreeBloomBackendMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/gdse70/agreebloom/AgreeBloomBackendMainApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse70.agreebloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgreeBloomBackendMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
